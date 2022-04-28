d5929124c8ea48ce195080e6a4fbbd775918e9d1
David Delarosa <xdavidel@gmail.com>
2022-04-28 17:50:35 +0300

    Tweaking control table

--- src/components/NamespacePage.tsx
+++ src/components/NamespacePage.tsx
@@ -12,6 +12,7 @@ import { Logger } from "../utils/logger";
 const { Component: { TableHead, TableRow, TableCell, Table, Spinner, Icon } } = Renderer;
 type Pod = Renderer.K8sApi.Pod;
 
+
 @observer
 export class NamespacePage extends React.Component {
     namespaceStore: Renderer.K8sApi.NamespaceStore = Renderer.K8sApi.apiManager.getStore(Renderer.K8sApi.namespacesApi) as Renderer.K8sApi.NamespaceStore;
@@ -89,13 +90,14 @@ export class NamespacePage extends React.Component {
         const controls = [];
 
         for (let control of this.activeClusterResult.result) {
+            const docUrl = `https://hub.armo.cloud/docs/${control.controlID.toLocaleLowerCase()}`
             controls.push(
                 <TableRow key={control.controlID} nowrap>
-                    <TableCell className="controlId">{control.controlID}</TableCell>
+                    <TableCell className="controlId"><a target="_blank" href={docUrl}>{control.controlID}</a></TableCell>
                     <TableCell className="controlName">{control.name} </TableCell>
-                    <TableCell className="controlDescription">{control.description}</TableCell>
+                    {/* <TableCell className="controlDescription">{control.description}</TableCell> */}
                     <TableCell className="failedResources">{control.failedResources}</TableCell>
-                    <TableCell className="excludedResources">{control.warningResources}</TableCell>
+                    {/* <TableCell className="excludedResources">{control.warningResources}</TableCell> */}
                     <TableCell className="allResources">{control.totalResources}</TableCell>
                     <TableCell className="riskScore">{control.score}%</TableCell>
                 </TableRow>
@@ -107,7 +109,7 @@ export class NamespacePage extends React.Component {
     }
 
     render() {
-        console.log(this.activeClusterResult.result);
+        Logger.debug(this.activeClusterResult.result);
 
         if (!this.activeClusterResult || !this.activeClusterResult.result) {
             return (
@@ -122,16 +124,13 @@ export class NamespacePage extends React.Component {
 
         return (
             <>
-                {/**<div>Selected Namespaces: {this.selectedNamespaces.map(ns => (<span>{ns}</span>))}</div>
-                <div>Pods: </div>{this.podList.map(pod => (<div>{pod}</div>))}
-         <div>Scaned Frameworks Count: {this.activeClusterResult.result.length}</div>**/}
-                <Table>
+                <Table tableId="controlReportsTable" selectable >
                     <TableHead>
                         <TableCell className="controlId">ID</TableCell>
                         <TableCell className="controlName">Control Name</TableCell>
-                        <TableCell className="controlDescription">Description</TableCell>
+                        {/* <TableCell className="controlDescription">Description</TableCell> */}
                         <TableCell className="failedResources">Failed Resources</TableCell>
-                        <TableCell className="excludedResources">Excluded Resources</TableCell>
+                        {/* <TableCell className="excludedResources">Excluded Resources</TableCell> */}
                         <TableCell className="allResources">All Resources</TableCell>
                         <TableCell className="riskScore">Risk Score</TableCell>
                     </TableHead>
