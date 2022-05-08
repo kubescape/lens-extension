6d4fd622efc158a29bc56dad11c3cb5886bedf77
David Delarosa <xdavidel@gmail.com>
2022-05-08 15:32:23 +0300

    Namespace must be specified

--- src/stores/KubescapeReportStore.ts
+++ src/stores/KubescapeReportStore.ts
@@ -8,8 +8,6 @@ const { Store } = Common;
 export class KubescapeReportStore extends Store.ExtensionStore<KubescapeReportStoreModel> {
     @observable scanResults: KubescapeClusterScanResult[] = [];
 
-    @observable resourceKindToStore: { [kind: string]: Renderer.K8sApi.KubeObjectStore<Renderer.K8sApi.KubeObject> } = {};
-
     @computed get activeCluster() {
         const activeEntity = Renderer.Catalog.catalogEntities.activeEntity;
         if (activeEntity && activeEntity instanceof Common.Catalog.KubernetesCluster) {
@@ -46,37 +44,14 @@ export class KubescapeReportStore extends Store.ExtensionStore<KubescapeReportSt
     }
 
     getStore = (kind, apiVersion) => {
-        switch (kind) {
-            case "Role":
-                return Renderer.K8sApi.apiManager.getStore(Renderer.K8sApi.roleApi) as Renderer.K8sApi.RolesStore;
-            case "Namespace":
-                return (Renderer.K8sApi.apiManager.getStore(Renderer.K8sApi.namespacesApi) as unknown) as Renderer.K8sApi.NamespaceStore;
-            case "Pod":
-                return Renderer.K8sApi.apiManager.getStore(Renderer.K8sApi.podsApi) as Renderer.K8sApi.PodsStore;
-            case "Deployment":
-                return Renderer.K8sApi.apiManager.getStore(Renderer.K8sApi.deploymentApi) as Renderer.K8sApi.DeploymentStore;
-            case "StatefulSet":
-                return Renderer.K8sApi.apiManager.getStore(Renderer.K8sApi.statefulSetApi) as Renderer.K8sApi.StatefulSetStore;
-            case "DaemonSet":
-                return Renderer.K8sApi.apiManager.getStore(Renderer.K8sApi.daemonSetApi) as Renderer.K8sApi.DaemonSetStore;
-            case "Secret":
-                return Renderer.K8sApi.apiManager.getStore(Renderer.K8sApi.secretsApi) as Renderer.K8sApi.SecretsStore;
-            case "Service":
-                return Renderer.K8sApi.apiManager.getStore(Renderer.K8sApi.serviceApi) as Renderer.K8sApi.ServiceStore;
-            case "VolumeClaim":
-                return Renderer.K8sApi.apiManager.getStore(Renderer.K8sApi.pvcApi) as Renderer.K8sApi.VolumeClaimStore;
-            case "ConfigMap":
-                return Renderer.K8sApi.apiManager.getStore(Renderer.K8sApi.configMapApi) as Renderer.K8sApi.ConfigMapsStore;
-        }
-
         return Renderer.K8sApi.apiManager.getStore(Renderer.K8sApi.apiManager.getApiByKind(kind, apiVersion)) as Renderer.K8sApi.KubeObjectStore<Renderer.K8sApi.KubeObject>;
     }
 
-    getKubeObject = async (kind: string, apiVersion: string, id: string): Promise<Renderer.K8sApi.KubeObject> => {
+    getKubeObject = async (namespace : string, kind: string, apiVersion: string, id: string): Promise<Renderer.K8sApi.KubeObject> => {
         const store = this.getStore(kind, apiVersion);
 
         if (!store.isLoaded) {
-            await store.loadAll();
+            await store.loadAll({ namespaces: [namespace], merge: false });
         }
 
         return store.getById(id);
